40edc55394f7d301bd5d7f05c975154e69775bef
darricklaidin <darricklaidin@gmail.com>
2023-06-15 21:40:46 +1000

    Initialize home, store, and cart page

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import NavBar from './NavBar';
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <h1>Shopping Cart</h1>
+      <button>Start browsing</button>
+      <button>(cart icon)</button>
+    </>
+  )
+}
+
+export default Home;
---
+++ src/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/store">Store</a></li>
+        <li><a href="/cart">0 (cart icon)</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
