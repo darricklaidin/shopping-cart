d9f8e0978a321bd6830bc5bca7c4d40d298b899d
darricklaidin <darricklaidin@gmail.com>
2023-06-15 21:40:13 +1000

    Add routing to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './css/App.css';
+import Home from './Home'
+import Store from './Store'
+import Cart from './Cart'
+
+const App = () => {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+        <Route path="/store" element={ <Store /> } />
+        <Route path="/cart" element={ <Cart /> } />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
