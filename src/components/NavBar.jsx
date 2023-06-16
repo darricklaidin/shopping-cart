ba3c89b4a71b48b5df0a89a10c648f11a37490a1
darricklaidin <darricklaidin@gmail.com>
2023-06-16 15:56:12 +1000

    Move existing and new components to sub directory

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import ShoppingCartIcon from './ShoppingCartIcon'
+import { useNavigate } from 'react-router-dom';
+
+const NavBar = (props) => {
+  const navigate = useNavigate();
+  
+  return (
+    <nav>
+      <ul>
+        <li onClick={() => navigate("/")}>Home</li>
+        <li onClick={() => navigate("/store")}>Store</li>
+        <li onClick={() => navigate("/cart")}>{props.numItemsInCart} <ShoppingCartIcon height="32px" width="32px"/></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
