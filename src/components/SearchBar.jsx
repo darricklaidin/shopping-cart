6c1f7d913d2b9b51db0f3f1258c63d57c84d9caf
darricklaidin <darricklaidin@gmail.com>
2023-06-17 02:46:46 +1000

    Remove search icon Filter automatically updates as user types in the input field anyways

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,8 @@ import SearchIcon from './SearchIcon'
 const SearchBar = (props) => {
   return (
     <div className='search-bar'>
-      <input className='search-field' type="text" placeholder="Search for an item" maxLength={90} onChange={props.updateSearch} />
-      <button className='search-button'><SearchIcon width="32px" height="32px" /></button>
+      <input className='search-field' type="text" name="search-field" placeholder="Search for an item" maxLength={90} onChange={props.updateSearch} />
+      {/* <button className='search-button'><SearchIcon width="32px" height="32px" /></button> */}
     </div>
   )
 }
